4f40c43d29fdb0bbc7ecbe436bed77eee9cb278a
URUSHIHARA Akihiro <a.urusihara@gmail.com>
2026-02-24 02:03:44 +0900

    Add tests for media download and thumbnail

--- core/src/jvmTest/kotlin/work/socialhub/kmatrix/apis/MediaTest.kt
+++ core/src/jvmTest/kotlin/work/socialhub/kmatrix/apis/MediaTest.kt
@@ -2,6 +2,8 @@ package work.socialhub.kmatrix.apis
 
 import kotlinx.coroutines.test.runTest
 import work.socialhub.kmatrix.AbstractTest
+import work.socialhub.kmatrix.api.request.media.MediaDownloadRequest
+import work.socialhub.kmatrix.api.request.media.MediaThumbnailRequest
 import work.socialhub.kmatrix.api.request.media.MediaUploadRequest
 import kotlin.test.Test
 
@@ -39,5 +41,78 @@ class MediaTest : AbstractTest() {
         val response = matrix().media().upload(request)
         println("=== Upload Media ===")
         println("  Content URI > ${response.data.contentUri}")
+    }
+
+    @Test
+    fun testDownloadMedia() = runTest {
+        // First upload a file
+        val uploadResponse = matrix().media().upload(
+            MediaUploadRequest().also {
+                it.fileName = "download-test.png"
+                it.contentType = "image/png"
+                it.bytes = MINIMAL_PNG
+            }
+        )
+        val contentUri = uploadResponse.data.contentUri
+        println("=== Download Media ===")
+        println("  Content URI > $contentUri")
+
+        // Parse mxc:// URI into serverName and mediaId
+        val mxcParts = contentUri.removePrefix("mxc://").split("/", limit = 2)
+        val serverName = mxcParts[0]
+        val mediaId = mxcParts[1]
+
+        try {
+            // Download the file
+            val bytes = matrix().media().download(
+                MediaDownloadRequest().also {
+                    it.serverName = serverName
+                    it.mediaId = mediaId
+                }
+            )
+            println("  Downloaded  > ${bytes.size} bytes")
+            assert(bytes.isNotEmpty())
+        } catch (e: Exception) {
+            // Some servers have deprecated /_matrix/media/v3/download
+            // in favor of /_matrix/client/v1/media/download
+            println("  Download not available: ${e.message}")
+        }
+    }
+
+    @Test
+    fun testThumbnail() = runTest {
+        // First upload an image
+        val uploadResponse = matrix().media().upload(
+            MediaUploadRequest().also {
+                it.fileName = "thumb-test.png"
+                it.contentType = "image/png"
+                it.bytes = MINIMAL_PNG
+            }
+        )
+        val contentUri = uploadResponse.data.contentUri
+        println("=== Thumbnail ===")
+        println("  Content URI > $contentUri")
+
+        // Parse mxc:// URI
+        val mxcParts = contentUri.removePrefix("mxc://").split("/", limit = 2)
+        val serverName = mxcParts[0]
+        val mediaId = mxcParts[1]
+
+        try {
+            val bytes = matrix().media().thumbnail(
+                MediaThumbnailRequest().also {
+                    it.serverName = serverName
+                    it.mediaId = mediaId
+                    it.width = 32
+                    it.height = 32
+                    it.method = "scale"
+                }
+            )
+            println("  Thumbnail   > ${bytes.size} bytes")
+            assert(bytes.isNotEmpty())
+        } catch (e: Exception) {
+            // Some servers may not support thumbnails for very small images
+            println("  Thumbnail not available: ${e.message}")
+        }
     }
 }
