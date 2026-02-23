61f829e6f0dc299b9c0ad4cff4abef450cdf6b74
URUSHIHARA Akihiro <a.urusihara@gmail.com>
2026-02-24 00:06:08 +0900

    Fix room alias URL encoding in DirectoryResource
    URL-encode room alias path parameter to properly handle
    the '#' character in aliases like #room:server.

--- core/src/commonMain/kotlin/work/socialhub/kmatrix/internal/DirectoryResourceImpl.kt
+++ core/src/commonMain/kotlin/work/socialhub/kmatrix/internal/DirectoryResourceImpl.kt
@@ -1,5 +1,6 @@
 package work.socialhub.kmatrix.internal
 
+import io.ktor.http.encodeURLPathPart
 import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
 import work.socialhub.khttpclient.HttpRequest
@@ -26,7 +27,7 @@ class DirectoryResourceImpl(
     ): Response<DirectoryResolveAliasResponse> {
         return proceed {
             HttpRequest()
-                .url("${uri}/_matrix/client/v3/directory/room/${roomAlias}")
+                .url("${uri}/_matrix/client/v3/directory/room/${roomAlias.encodeURLPathPart()}")
                 .accept(MediaType.JSON)
                 .get()
         }
@@ -49,7 +50,7 @@ class DirectoryResourceImpl(
                 )
             )
             HttpRequest()
-                .url("${uri}/_matrix/client/v3/directory/room/${roomAlias}")
+                .url("${uri}/_matrix/client/v3/directory/room/${roomAlias.encodeURLPathPart()}")
                 .header(AUTHORIZATION, bearerToken())
                 .accept(MediaType.JSON)
                 .json(body)
@@ -68,7 +69,7 @@ class DirectoryResourceImpl(
     ): ResponseUnit {
         return proceedUnit {
             HttpRequest()
-                .url("${uri}/_matrix/client/v3/directory/room/${roomAlias}")
+                .url("${uri}/_matrix/client/v3/directory/room/${roomAlias.encodeURLPathPart()}")
                 .header(AUTHORIZATION, bearerToken())
                 .accept(MediaType.JSON)
                 .delete()
