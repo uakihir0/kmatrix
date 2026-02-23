36227c1ac7184fba7851dae17a1cc28014d0693c
URUSHIHARA Akihiro <a.urusihara@gmail.com>
2026-02-24 01:59:21 +0900

    Add test for updateDevice

--- core/src/jvmTest/kotlin/work/socialhub/kmatrix/apis/DevicesTest.kt
+++ core/src/jvmTest/kotlin/work/socialhub/kmatrix/apis/DevicesTest.kt
@@ -2,6 +2,7 @@ package work.socialhub.kmatrix.apis
 
 import kotlinx.coroutines.test.runTest
 import work.socialhub.kmatrix.AbstractTest
+import work.socialhub.kmatrix.api.request.devices.DevicesUpdateRequest
 import kotlin.test.Test
 
 class DevicesTest : AbstractTest() {
@@ -30,6 +31,41 @@ class DevicesTest : AbstractTest() {
             println("  Display Name > ${response.data.displayName}")
             println("  Last Seen IP > ${response.data.lastSeenIp}")
             println("  Last Seen TS > ${response.data.lastSeenTs}")
+        }
+    }
+
+    @Test
+    fun testUpdateDevice() = runTest {
+        val devices = matrix().devices().getDevices()
+        if (devices.data.devices.isNotEmpty()) {
+            val device = devices.data.devices.first()
+            val deviceId = device.deviceId
+            val originalName = device.displayName
+
+            // Update device display name
+            val testName = (originalName ?: "kmatrix-device") + " (test)"
+            matrix().devices().updateDevice(
+                DevicesUpdateRequest().also {
+                    it.deviceId = deviceId
+                    it.displayName = testName
+                }
+            )
+            println("=== Update Device ===")
+            println("  Device ID     > $deviceId")
+            println("  Updated Name  > $testName")
+
+            // Verify update
+            val updated = matrix().devices().getDevice(deviceId)
+            println("  Verified Name > ${updated.data.displayName}")
+
+            // Restore original name
+            matrix().devices().updateDevice(
+                DevicesUpdateRequest().also {
+                    it.deviceId = deviceId
+                    it.displayName = originalName ?: ""
+                }
+            )
+            println("  Restored Name > $originalName")
         }
     }
 }
