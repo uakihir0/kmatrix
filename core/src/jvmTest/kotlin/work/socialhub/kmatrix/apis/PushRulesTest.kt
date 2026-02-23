631df3b8867591df892ce65e6184ea6ef6a6968e
URUSHIHARA Akihiro <a.urusihara@gmail.com>
2026-02-24 02:01:37 +0900

    Add test for setEnabled push rule

--- core/src/jvmTest/kotlin/work/socialhub/kmatrix/apis/PushRulesTest.kt
+++ core/src/jvmTest/kotlin/work/socialhub/kmatrix/apis/PushRulesTest.kt
@@ -2,6 +2,7 @@ package work.socialhub.kmatrix.apis
 
 import kotlinx.coroutines.test.runTest
 import work.socialhub.kmatrix.AbstractTest
+import work.socialhub.kmatrix.api.request.push.PushRulesSetEnabledRequest
 import kotlin.test.Test
 
 class PushRulesTest : AbstractTest() {
@@ -32,5 +33,45 @@ class PushRulesTest : AbstractTest() {
         )
         println("=== Push Rule Enabled ===")
         println("  .m.rule.master enabled > ${response.data.enabled}")
+    }
+
+    @Test
+    fun testSetEnabled() = runTest {
+        // Get current state of .m.rule.master
+        val before = matrix().pushRules().getEnabled(
+            "global", "override", ".m.rule.master"
+        )
+        val originalEnabled = before.data.enabled
+        println("=== Set Push Rule Enabled ===")
+        println("  Original > $originalEnabled")
+
+        // Toggle the enabled state
+        val newEnabled = !originalEnabled
+        matrix().pushRules().setEnabled(
+            PushRulesSetEnabledRequest().also {
+                it.scope = "global"
+                it.kind = "override"
+                it.ruleId = ".m.rule.master"
+                it.enabled = newEnabled
+            }
+        )
+        println("  Updated  > $newEnabled")
+
+        // Verify the change
+        val after = matrix().pushRules().getEnabled(
+            "global", "override", ".m.rule.master"
+        )
+        println("  Verified > ${after.data.enabled}")
+
+        // Restore original state
+        matrix().pushRules().setEnabled(
+            PushRulesSetEnabledRequest().also {
+                it.scope = "global"
+                it.kind = "override"
+                it.ruleId = ".m.rule.master"
+                it.enabled = originalEnabled
+            }
+        )
+        println("  Restored > $originalEnabled")
     }
 }
